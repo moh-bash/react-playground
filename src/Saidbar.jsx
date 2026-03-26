553a995a453bda487f9ee7fba620177784b4f4c3
Mohamad Bashyr <mbashirkuja@gmail.com>
2026-03-26 10:37:08 +0300

    lopButton

--- src/Saidbar.jsx
+++ src/Saidbar.jsx
@@ -1,21 +1,30 @@
-import './App.css'
-import { Button } from './Button'
+import "./App.css";
+import { Button } from "./Button";
 export function Saidbar() {
-    const buttonData =[
-        { id: "1", text: "Button 1" },
-    ]
-    return (
-        <div className="saidbar box">
-            <Button text="Button 1">
-                <p>😍😍😍</p>
-            </Button>
-            <Button text="Button 2">
-                <img src="https://cdn.mos.cms.futurecdn.net/ARWms77gyVZ5u2MZkQqcKc-1256-80.jpg.webp" alt="icon" width={70} />
-            </Button>
-            <Button text="Button 3"></Button>
-            <Button text="Button 4"></Button>
-        </div>
-    )
+  const buttonData = [
+    { id: "1", text: "Button 1", c: <p>😍😍😍 </p> },
+    {
+      id: "2",
+      text: "Button 2",
+      c: (
+        <img
+          src="https://cdn.mos.cms.futurecdn.net/ARWms77gyVZ5u2MZkQqcKc-1256-80.jpg.webp"
+          alt="icon"
+          width={70}
+        />
+      ),
+    },
+    { id: "3", text: "Button 3", c: <p>😍😍😍 </p> },
+    { id: "4", text: "Button 4", c: <p>😍😍😍 </p> },
+  ];
+  const buttonList = buttonData.map((button) => (
+    <Button key={button.id} text={button.text}>
+      {button.c}
+    </Button>
+  ));
+  return (
+    <div className="saidbar box">
+      {buttonList}
+    </div>
+  );
 }
-
-
