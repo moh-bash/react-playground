d88d320c27d0938455058e8836d7a20e523c4770
Mohamad Bashyr <mbashirkuja@gmail.com>
2026-03-26 20:33:52 +0300

    stase

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import './App.css'
-import { Header } from './Header'
-import { Sction } from './Sction'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function Scoreboard() {
+  const [player, setPlayer] = useState({
+    firstName: "Ranjani",
+    lastName: "Shettar",
+    score: 10,
+  });
+
+  function handlePlusClick() {
+    setPlayer({
+      ...player,
+      score: player.score + 1,
+    })
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value,
+    });
+  }
 
   return (
-    <>
-      <Header />
-      <Sction />
-    </>
-  )
+    <div className="Sction">
+      <label>
+        Score: <b>{player.score}</b>{" "}
+        <button onClick={handlePlusClick}>+1</button>
+      </label>
+      <label>
+        First name:
+        <input value={player.firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name:
+        <input value={player.lastName} onChange={handleLastNameChange} />
+      </label>
+    </div>
+  );
 }
-
-export default App
