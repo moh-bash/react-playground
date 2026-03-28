beb163e08bc6e6ed74e0e4d15c5b225af3f5b0de
Mohamad Bashyr <mbashirkuja@gmail.com>
2026-03-28 21:26:22 +0300

    feat: implement ShoppingCart component and remove unused components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-import './App.css'
-import { Header } from './Header'
-import { Sction } from './Sction'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
+
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    const newProducts = products.map((product) => {
+      if(product.id === productId){
+        return{ ...product, count: product.count + 1 }
+      }else{
+        return product;
+      }
+    })
+    setProducts(newProducts);
+  }
 
   return (
-    <>
-      <Header />
-      <Sction />
-    </>
-  )
+    <div className="Sction">
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} (<b>{product.count}</b>)
+            <button
+              onClick={() => {
+                handleIncreaseClick(product.id);
+              }}
+            >
+              +
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default App
