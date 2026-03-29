de52d384f251540eb5b3ffd814a00e2d11ebffb9
Mohamad Bashyr <mbashirkuja@gmail.com>
2026-03-29 23:28:29 +0300

    Queueing a Series of State Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import './App.css'
-import { Header } from './Header'
-import { Sction } from './Sction'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
+  async function handleClick() {
+    setPending((prev) => prev + 1);
+    await delay(3000);
+    setPending((prev) => prev - 1);
+    setCompleted((prev) => prev + 1);
+  }
 
   return (
-    <>
-      <Header />
-      <Sction />
-    </>
-  )
+    <div className="Sction">
+      <h3>Pending: {pending}</h3>
+      <h3>Completed: {completed}</h3>
+      <button onClick={handleClick}>Buy</button>
+    </div>
+  );
+
+  function delay(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
 }
 
-export default App
+export default App;
